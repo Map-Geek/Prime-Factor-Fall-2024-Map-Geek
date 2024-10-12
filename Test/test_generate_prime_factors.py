b933012ba5825212ae71a4b85eac85e8c67191cb
Map-Geek <geetha.ramesh@dcmail.ca>
2024-10-11 19:38:58 -0700

    Step 1 test for non-integer input

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,1 +1,28 @@
+"""
+Python Development II Assignment 3: Prime Factors
+Geetha Ramesh
 
+test_generate_prime_factors.py
+
+Unit tests for the generate_prime_factors function.
+
+This module contains test cases for validating the behavior of the
+generate_prime_factors function, which generates a list of prime
+factors for a given integer.
+"""
+
+import pytest
+from prime import generate_prime_factors
+
+
+def test_datatype_not_integer():
+    """
+    Test to check if generate_prime_factors() function
+    raises a ValueError for any input that is not an integer.
+    """
+    with pytest.raises(ValueError):
+        generate_prime_factors("number")
+    with pytest.raises(ValueError):
+        generate_prime_factors(2.4)
+    with pytest.raises(ValueError):
+        generate_prime_factors(True)
--- prime.py
+++ prime.py
@@ -1,3 +1,29 @@
 """
-prime.py -- Write the application code here
+Python Development II Assignment 3: Prime Factors
+Geetha Ramesh
+
+prime.py
+
+This module provides the `generate_prime_factors` function, which takes an integer input
+and returns a list of its prime factors. It raises a ValueError if the input is not a valid
+integer (including booleans and non-numeric types).
+
+Functions:
+    - generate_prime_factors(number: int) -> list:
+        Generates a list of prime factors of the input integer.
+
+Exceptions:
+    - Raises ValueError for non-integer inputs, including booleans and non-numeric types.
 """
+
+
+def generate_prime_factors(number):
+    """
+    Generate a list of prime factors for a given integer.
+
+    :param number: the integer for which to find prime factors
+    :returns: a list of prime factors of the input integer
+    :raises: ValueError if input is not integer.
+    """
+    if not isinstance(number, int) or isinstance(number, bool):
+        raise ValueError("The input value must be an integer")
